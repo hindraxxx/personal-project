1eb5c9fa5bc21f4563cb5bf59302a171b4172a6c
Liem Hindra Sanjaya <liem.sanjaya@gojek.com>
2026-05-16 15:37:53 +0700

    push latest script

--- test/PlainJavaSmokeTest.java
+++
@@ -1,9 +0,0 @@
-import main_runner.Main;
-
-public class PlainJavaSmokeTest {
-
-  public static void main(String[] args) {
-    Main.main(new String[]{});
-    System.out.println("Plain Java smoke test passed");
-  }
-}
